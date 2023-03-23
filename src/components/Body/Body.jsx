11f3d85a243f368846f43947cdc089908c57892f
Aksh-007 <akshaybarapatre54@gmail.com>
2023-03-24 02:03:25 +0530

    data mapped into card

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import SearchBar from "./SearchBar";
+import {  API_URL,restaurantList } from "../constant/constant.js";
 
 const Body = () => {
+//   const [allRestaurant, setAllRestaurant] = useState(restaurantList);
+
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    try {
+      const data = await fetch(
+        API_URL
+      );
+      const json = await data.json();
+      console.log(json);
+    //   setAllRestaurant(json)
+    //   setAllRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    //   setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    } catch (error) {
+      console.log(error);
+      console.log(error.message);
+    }
+  }
+
+
   return (
     <>
       <SearchBar />
-        <div className="flex row gap-10 flex-wrap p-10">
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-        </div>
+      <div className="flex row gap-10 flex-wrap p-10 items-center">
+        {
+            restaurantList.map((restaurant)=>{
+                return(
+                    <RestaurantCard 
+                    key={restaurant?.data?.uuid}
+                    {...restaurant.data}
+                    />
+                )
+            })
+        }
+       
+      </div>
     </>
   );
 };
